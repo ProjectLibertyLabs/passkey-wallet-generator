4501f41fd28584222f040cc09be2119bccee6352
Aramik <aramikm@gmail.com>
2024-07-17 10:44:27 -0700

    init signing payload

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,9 +1,31 @@
+import { Keyring } from '@polkadot/api';
+import {cryptoWaitReady, mnemonicGenerate} from '@polkadot/util-crypto'
+import { hexToU8a, u8aToHex, u8aWrapBytes } from '@polkadot/util';
+
+let SEED : String;
+cryptoWaitReady().then(() => {
+    SEED = mnemonicGenerate();
+    console.log(SEED);
+});
+
 export const generateAndReturnPublicKey = async () => {
-  // @TODO
+    SEED = mnemonicGenerate();
+    // console.log("updated: ", SEED);
+    const keyring = new Keyring({ type: 'sr25519' });
+    const pair = keyring.addFromMnemonic(SEED);
+    localStorage.setItem("mnemonic", SEED);
+    return u8aToHex(pair.publicKey)
 };
 
-export const signPasskeyPublicKey = async (publicKey: string) => {
-  // @TODO
+export const signPasskeyPublicKey = async (compressedPublicKeyHex: string) => {
+    const keyring = new Keyring({ type: 'sr25519' });
+    const pair = keyring.addFromMnemonic(SEED);
+
+    let payloadHex = hexToU8a(compressedPublicKeyHex);
+    let wrapped = u8aWrapBytes(payloadHex);
+    let signature =  u8aToHex(pair.sign(wrapped));
+    console.log(signature);
+    return signature;
 };
 
 export const triggerSeedPhraseDownload = () => {
