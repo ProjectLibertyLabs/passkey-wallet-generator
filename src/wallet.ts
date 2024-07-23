78a3c0b9bdd678afaa96b84f3d84c80b90538ad8
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-07-23 08:48:58 -0500

    return encoding and addess with seed object

--- src/wallet.ts
+++ src/wallet.ts
@@ -5,7 +5,7 @@ import { hexToU8a, isHex, u8aToHex, u8aWrapBytes } from '@polkadot/util';
 const keyring = new Keyring({ type: 'sr25519' });
 
 let isReady = false;
-let SEED: string = '';
+let SEED = '';
 cryptoWaitReady().then(() => {
   isReady = true;
 });
@@ -27,8 +27,8 @@ export const signPasskeyPublicKey = async (compressedPublicKeyHex: string) => {
     throw new Error('Input is not a valid hex!');
   }
   const pair = keyring.createFromUri(SEED);
-  let payloadHex = hexToU8a(compressedPublicKeyHex);
-  let wrapped = u8aWrapBytes(payloadHex);
+  const payloadHex = hexToU8a(compressedPublicKeyHex);
+  const wrapped = u8aWrapBytes(payloadHex);
   return u8aToHex(pair.sign(wrapped));
 };
 
@@ -36,8 +36,17 @@ export const triggerSeedPhraseDownload = () => {
   if (SEED.length === 0) {
     throw new Error('Keypair is not generated!');
   }
-  let seedData = JSON.stringify({ seed: SEED, whenCreated: new Date().getTime() });
-  return seedData;
+  const pair = keyring.createFromUri(SEED);
+  const seedData = {
+    seed: SEED,
+    whenCreated: new Date().getTime(),
+    encoding: {
+      content: ['pkcs8', 'sr25519'],
+    },
+    address: pair.address,
+  };
+
+  return JSON.stringify(seedData);
 };
 
 export const testHelperResetState = () => {
